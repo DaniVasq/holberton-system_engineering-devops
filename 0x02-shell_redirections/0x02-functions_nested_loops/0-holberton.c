f54333a9bf11ccd8862797f069c54a90eb3b942b
DaniVasq <danivasq50@gmail.com>
2019-02-12 14:44:35 +0000

    _putchar

---
+++ 0x02-shell_redirections/0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+# include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar(48);
+	_putchar(49);
+	_putchar(10);
+	_putchar(50);
+	_putchar(51);
+	_putchar(13);
+	return (0);
+}
