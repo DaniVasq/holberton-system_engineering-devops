c3d1f230a78206d7200a0017f4a01e5ad8565a5a
DaniVasq <danivasq50@gmail.com>
2019-12-10 04:06:30 +0000

    Fixing syntax

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file in /tmp with Puppet.
 file { '/tmp/holberton':
-     path	=> '/tmp/holberton',
-     ensure	=> file,
-     mode	=> '0744',
-     owner 	=> 'www-data',
-     group	=> 'www-data',
-     content	=> 'I love Puppet'
+  path    => '/tmp/holberton',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
